03cf2ae4e39de32874b7f0f6ca863e1bec066401
= <darren.wainwright@purolator.com>
2022-12-07 08:33:20 -0500

    Day 1 part 2

--- Day-1/Program.cs
+++ Day-1/Program.cs
@@ -1,4 +1,7 @@
-﻿int max = 0;
+﻿// AOC '22 Day 1
+
+// Part 1 - find calorie count for elf carrying the most
+int max = 0;
 int temp = 0;
 File.ReadAllLines("input.txt").ToList().ForEach(line =>
 {
@@ -10,4 +13,21 @@ File.ReadAllLines("input.txt").ToList().ForEach(line =>
         temp = 0;
     }
 });
-Console.WriteLine($"{max} total calories carried by the elf with the most");
+Console.WriteLine($"Part 1: {max} total calories carried by the elf with the most");
+
+
+// Part 2 - find calorie total for top 3
+temp = 0;
+List<int> calsPerElf = new List<int>();
+File.ReadAllLines("input.txt").ToList().ForEach(line =>
+{
+    if (line != string.Empty)
+        temp += int.Parse(line);
+    else
+    {
+        calsPerElf.Add(temp);
+        temp = 0;
+    }
+});
+var top3Total = calsPerElf.OrderByDescending(l => l).Take(3).Sum();
+Console.WriteLine($"Part 2: {top3Total} total calories carried by the top 3 elves");
