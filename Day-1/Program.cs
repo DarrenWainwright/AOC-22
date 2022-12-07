43481c99aebafc1947d83a42d8317bb7235f44b2
= <darren.wainwright@purolator.com>
2022-12-07 08:10:09 -0500

    Day 1

---
+++ Day-1/Program.cs
@@ -0,0 +1,13 @@
+﻿int max = 0;
+int temp = 0;
+File.ReadAllLines("input.txt").ToList().ForEach(line =>
+{
+    if (line != string.Empty)
+        temp += int.Parse(line);
+    else
+    {
+        max = temp > max ? temp : max;
+        temp = 0;
+    }
+});
+Console.WriteLine($"{max} calories carried by an elf");
